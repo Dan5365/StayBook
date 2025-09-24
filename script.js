85fc705cae4986082d5fea3b3885798b6e069d9d
KeyCha <adilbekuly623@gmail.com>
2025-09-24 19:26:29 +0500

    Added script.js and updated styles and html pages

---
+++ script.js
@@ -0,0 +1,17 @@
+  const registerBox = document.getElementById('register-box');
+  const loginBox = document.getElementById('login-box');
+  const showLogin = document.getElementById('show-login');
+  const showRegister = document.getElementById('show-register');
+
+  showLogin.addEventListener('click', function (e) {
+    e.preventDefault();
+    registerBox.style.display = 'none';
+    loginBox.style.display = 'block';
+  });
+
+  showRegister.addEventListener('click', function (e) {
+    e.preventDefault();
+    loginBox.style.display = 'none';
+    registerBox.style.display = 'block';
+  });
+
