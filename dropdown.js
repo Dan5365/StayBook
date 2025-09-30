77509963469ae045bd45a03ef3d19c8e2730639d
KeyCha <adilbekuly623@gmail.com>
2025-09-30 12:02:03 +0500

    adding grid templates, clean code, sidebar and mobile adaptation, etc

--- dropdown.js
+++ dropdown.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".navbar__menu");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const toggle = document.querySelector(".menu-toggle");
+//     const menu = document.querySelector(".navbar__menu");
 
-    toggle.addEventListener("click", () => {
-      menu.classList.toggle("show");
-    });
-  });
+//     toggle.addEventListener("click", () => {
+//       menu.classList.toggle("show");
+//     });
+//   });
